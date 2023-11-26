e5cf4e87f0eff32fed77c104ca77c817f997dc22
Kennith Mann <kmann@etherpunk.com>
2023-11-25 19:52:33 -0600

    Adding confirm dialog Adding confirm delete to recurring transaction
    fixing bug with create/update (I had it swapped)

--- v7/MoneyRegister/Components/Pages/Util/DialogUtil.cs
+++ v7/MoneyRegister/Components/Pages/Util/DialogUtil.cs
@@ -6,7 +6,7 @@ namespace MoneyRegister.Components.Pages.Util;
 
 public class DialogUtil
 {
-    public static async Task<DialogResult> ShowAccountDialog(IDialogService dialogService, bool isNew, RecurringTransaction recurringTransaction)
+    public static async Task<DialogResult> ShowAccountDialogAsync(IDialogService dialogService, bool isNew, RecurringTransaction recurringTransaction)
     {
         var parameters = new DialogParameters<RecurringTransactionDialog>
         {
@@ -23,7 +23,7 @@ public class DialogUtil
         return await dialog.Result;
     }
 
-    public static async Task<DialogResult> ShowCategoryDialog(IDialogService dialogService, bool isNew, Category category)
+    public static async Task<DialogResult> ShowCategoryDialogAsync(IDialogService dialogService, bool isNew, Category category)
     {
         var parameters = new DialogParameters<CategoryDialog>
         {
@@ -37,6 +37,22 @@ public class DialogUtil
         };
 
         var dialog = await dialogService.ShowAsync<CategoryDialog>(isNew ? "New Category" : "Edit Category", parameters, options);
+        return await dialog.Result;
+    }
+
+    public static async Task<DialogResult> ShowConfirmDialogAsync(IDialogService dialogService, string title, string buttonText, string contentText, Color color)
+    {
+        var parameters = new DialogParameters<ConfirmDialog>
+        {
+            {x => x.ContentText, contentText},
+            {x => x.ButtonText, buttonText },
+            {x => x.Color, color }
+        };
+
+        var options = new DialogOptions() { CloseButton = true, MaxWidth = MaxWidth.ExtraSmall };
+
+        var dialog = await dialogService.ShowAsync<ConfirmDialog>(title, parameters, options);
+
         return await dialog.Result;
     }
 }
