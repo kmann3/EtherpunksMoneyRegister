8aa3cd77a9e513d6432b55294e531d0e1cd309c4
Kennith Mann <kmann@etherpunk.com>
2023-12-01 16:24:15 -0600

    changing to crlf

--- v7/MoneyRegister/Data/Services/RecurringTransactionService.cs
+++ v7/MoneyRegister/Data/Services/RecurringTransactionService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MoneyRegister.Components.Pages;
 using MoneyRegister.Data.Entities;
 namespace MoneyRegister.Data.Services;
 
@@ -13,8 +14,21 @@ public class RecurringTransactionService(ApplicationDbContext context)
             .Include(x => x.Categories)
             .Include(x => x.FrequencyLookup)
             .Include(x => x.TransactionTypeLookup)
+            .Include(x => x.PreviousTransactions) // TBI: This might be a bad idea since we only use it for a count. Ideally we should just pull the count. Will need to make a custom class for this though.
             .OrderBy(x => x.TransactionTypeLookup.Name).ThenBy(x => x.Name)
             .ToListAsync();
+    }
+
+    public async Task<RecurringTransaction> GetRecurringTransactionAsync(Guid id)
+    {
+        return await _context.RecurringTransactions
+                .Include(x => x.Group)
+                .Include(x => x.Categories)
+                .Include(x => x.FrequencyLookup)
+                .Include(x => x.TransactionTypeLookup)
+                .Include(x => x.PreviousTransactions)
+            .Where(x => x.Id == id)
+            .SingleAsync();
     }
 
     public async Task CreateRecurringTransactionAsync(RecurringTransaction recurringTransaction)
