9bb619d65e51192a5844e335cb8efb54f35bc91d
Kennith Mann <kmann@etherpunk.com>
2023-12-10 04:41:33 -0600

    Work on TransactionDialog and makign new components for it

--- v7/MoneyRegister/Data/Entities/Transaction.cs
+++ v7/MoneyRegister/Data/Entities/Transaction.cs
@@ -71,7 +71,7 @@ public class Transaction : BasicTable<Transaction>, IEntityTypeConfiguration<Tra
     [JsonIgnore]
     public List<Link_Category_Transaction> Link_Category_Transactions { get; } = new List<Link_Category_Transaction>();
     [JsonIgnore]
-    public List<Category> Categories { get; } = new();
+    public List<Category> Categories { get; set; } = new();
     [JsonIgnore]
 
     public List<TransactionFile> Files { get; set; } = new();
