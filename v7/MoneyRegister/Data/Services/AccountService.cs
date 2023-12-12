3806dbff068ddf41f0ec85cb0968fce3ba8728a5
Kennith Mann <kmann@etherpunk.com>
2023-12-12 08:21:52 -0600

    Cleaning up code

--- v7/MoneyRegister/Data/Services/AccountService.cs
+++ v7/MoneyRegister/Data/Services/AccountService.cs
@@ -1,23 +1,37 @@
-﻿namespace MoneyRegister.Data.Services;
+﻿using Microsoft.EntityFrameworkCore;
+using MoneyRegister.Data.Entities;
 
-using Data.Entities;
-using Microsoft.EntityFrameworkCore;
-
+namespace MoneyRegister.Data.Services;
+/// <summary>
+/// Service for accessing account information.
+/// </summary>
+/// <param name="context"></param>
 public class AccountService(ApplicationDbContext context)
 {
     private ApplicationDbContext _context = context;
 
     private List<Account> _accounts = new();
 
+    /// <summary>
+    /// Gets a list of all the accounts.
+    /// This should be generally used for drop-downs.
+    /// </summary>
+    /// <returns></returns>
     public async Task<List<Account>> GetAllAccountsAsync()
     {
         if (_accounts.Count == 0)
         {
             _accounts = await _context.Accounts.OrderBy(x => x.Name).ToListAsync();
         }
+
         return _accounts;
     }
 
+    /// <summary>
+    /// Gets an accounts information including transactions and those transactions categories.
+    /// </summary>
+    /// <param name="accountId">The guid of the account to get.</param>
+    /// <returns>Returns an `Account` that is fully populated.</returns>
     public async Task<Account> GetAccountDetailsAsync(Guid accountId)
     {
         var data = await _context.Accounts
@@ -29,8 +43,7 @@ public class AccountService(ApplicationDbContext context)
                 Account = x,
                 Transactions = x.Transactions
                     .Where(x => x.DeletedOnUTC == null)
-                    //.Take(100)
-                    .ToList()
+                    .ToList(),
             })
             .SingleAsync();
 
@@ -39,32 +52,16 @@ public class AccountService(ApplicationDbContext context)
         Account returnData = data.Account;
         returnData.Transactions = data.Transactions;
 
-        foreach (var transaction in data.Transactions)
-        {
-            //await _context.Entry(transaction).Collection(x => x.Categories).LoadAsync();
-            Console.Write(transaction.Name + "--");
-            if (transaction.Categories.Count > 0)
-            {
-                Console.WriteLine("Has it: " + transaction.Categories.First().Name);
-            }
-            if (transaction.Categories.Count > 0)
-            {
-                foreach (var cat in transaction.Categories)
-                {
-                    Console.Write(cat.Name + ":");
-                }
-            }
-            else
-            {
-                Console.Write("none");
-            }
-
-            Console.WriteLine();
-        }
-
         return returnData;
     }
 
+    /// <summary>
+    /// Re-calculates the balance of all transactions and pending account information.
+    /// This should really only be used in very rare situations such as manually messing with the database.
+    /// The starting balance is where everything starts from - so that is required to be correct.
+    /// </summary>
+    /// <param name="account">The guid of the account to re-balance.</param>
+    /// <returns>Returns nothing.</returns>
     public async Task RecalculateAccountAsync(Account account)
     {
         var accountTransactions = _context.Transactions.Where(x => x.AccountId == account.Id).ToList();
