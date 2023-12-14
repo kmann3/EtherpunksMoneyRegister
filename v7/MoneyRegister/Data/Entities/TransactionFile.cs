da61edf14c96f41e4049dfbfe2b545d8b2b69c8f
Kennith Mann <kmann@etherpunk.com>
2023-12-13 23:38:28 -0600

    Fixing ContentType

--- v7/MoneyRegister/Data/Entities/TransactionFile.cs
+++ v7/MoneyRegister/Data/Entities/TransactionFile.cs
@@ -21,6 +21,7 @@ public class TransactionFile : BasicTable<TransactionFile>, IEntityTypeConfigura
     public Guid TransactionId { get; set; }
 
     public string Filename { get; set; } = string.Empty;
+    public string ContentType { get; set; } = string.Empty;
 
     public string Notes { get; set; } = string.Empty;
 
