781b91c91aa15d22ac67f99bd74f777a6b40b0fd
Kennith Mann <kmann@etherpunk.com>
2024-02-05 11:46:52 -0600

    Progress on MainWindow. Adding ViewModel starting stuffs

--- v9/MannsMoneyRegister/MainWindow.xaml.cs
+++ v9/MannsMoneyRegister/MainWindow.xaml.cs
@@ -21,7 +21,7 @@ public partial class MainWindow : Window
     private List<TestITem> testing = new();
     public MainWindow()
     {
-        InitializeComponent();
+        this.InitializeComponent();
         testing.Add(new TestITem());
         testing.Add(new TestITem());
         testing.Add(new TestITem());
@@ -29,11 +29,53 @@ public partial class MainWindow : Window
         testing.Add(new TestITem());
         testing.Add(new TestITem());
 
-        DataGridTransactions.ItemsSource = testing;
+        Trace.WriteLine($"{RibbonTest.Background.ToString()}");
+
+        dataGridTransactions.ItemsSource = testing;
     }
 
-    private void RibbonGallery_SelectionChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+    private void ribbonComboBox_Dashboard_AccountSelection_SelectionChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
     {
-        Trace.WriteLine($"What?");
+
+    }
+
+    private void ribbonComboBox_Dashboard_DayCount_SelectionChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+    {
+
+    }
+
+    private void ribbonButton_Dashboard_CustomRange_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void ribbonButton_Dashboard_NewTransaction_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void ribbonButton_Dashboard_NewAccount_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void ribbonCheckBox_Dashboard_Autoload_Checked(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void ribbonButton_Dashboard_ReserveTransaction_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void dataGridTransactions_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+
+    }
+
+    private void buttonSaveTransaction_Click(object sender, RoutedEventArgs e)
+    {
+
     }
 }
