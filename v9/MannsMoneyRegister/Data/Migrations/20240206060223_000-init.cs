b25ac7c113886981d1c61f0053a2133a8775ac6b
Kennith Mann <kmann@etherpunk.com>
2024-02-06 00:02:45 -0600

    Applying migrations

---
+++ v9/MannsMoneyRegister/Data/Migrations/20240206060223_000-init.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MannsMoneyRegister.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class _000init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Accounts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 255, nullable: false),
+                    StartingBalance = table.Column<decimal>(type: "TEXT", precision: 18, scale: 2, nullable: false),
+                    CurrentBalance = table.Column<decimal>(type: "TEXT", precision: 18, scale: 2, nullable: false),
+                    OutstandingBalance = table.Column<decimal>(type: "TEXT", precision: 18, scale: 2, nullable: false),
+                    OutstandingItemCount = table.Column<int>(type: "INTEGER", nullable: false),
+                    AccountNumber = table.Column<string>(type: "TEXT", nullable: false),
+                    InterestRate = table.Column<decimal>(type: "TEXT", precision: 18, scale: 2, nullable: false),
+                    Notes = table.Column<string>(type: "TEXT", nullable: false),
+                    LoginUrl = table.Column<string>(type: "TEXT", nullable: false),
+                    LastBalancedUTC = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    CreatedOnUTC = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accounts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 255, nullable: false),
+                    CreatedOnUTC = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RecurringTransactionGroups",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 255, nullable: false),
+                    CreatedOnUTC = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RecurringTransactionGroups", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RecurringTransactions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 255, nullable: false),
+                    NextDueDate = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    Amount = table.Column<decimal>(type: "TEXT", precision: 18, scale: 2, nullable: false),
+                    Notes = table.Column<string>(type: "TEXT", nullable: false),
+                    BankTransactionText = table.Column<string>(type: "TEXT", nullable: false),
+                    BankTransactionRegEx = table.Column<string>(type: "TEXT", nullable: false),
+                    RecurringFrequencyType = table.Column<string>(type: "TEXT", nullable: false),
+                    FrequencyValue = table.Column<int>(type: "INTEGER", nullable: true),
+                    FrequencyDayOfWeekValue = table.Column<int>(type: "INTEGER", nullable: true),
+                    FrequencyDateValue = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    RecurringTransactionGroupId = table.Column<Guid>(type: "TEXT", nullable: true),
+                    TransactionType = table.Column<string>(type: "TEXT", nullable: false),
+                    CreatedOnUTC = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RecurringTransactions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RecurringTransactions_RecurringTransactionGroups_RecurringTransactionGroupId",
+                        column: x => x.RecurringTransactionGroupId,
+                        principalTable: "RecurringTransactionGroups",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AccountTransactions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 255, nullable: false),
+                    Amount = table.Column<decimal>(type: "TEXT", precision: 18, scale: 2, nullable: false),
+                    AccountId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    TransactionType = table.Column<string>(type: "TEXT", nullable: false),
+                    TransactionPendingUTC = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    TransactionClearedUTC = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    Balance = table.Column<decimal>(type: "TEXT", precision: 18, scale: 2, nullable: false),
+                    Notes = table.Column<string>(type: "TEXT", nullable: false),
+                    RecurringTransactionId = table.Column<Guid>(type: "TEXT", nullable: true),
+                    DueDate = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    ConfirmationNumber = table.Column<string>(type: "TEXT", nullable: false),
+                    BankTransactionText = table.Column<string>(type: "TEXT", nullable: false),
+                    CreatedOnUTC = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AccountTransactions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AccountTransactions_Accounts_AccountId",
+                        column: x => x.AccountId,
+                        principalTable: "Accounts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AccountTransactions_RecurringTransactions_RecurringTransactionId",
+                        column: x => x.RecurringTransactionId,
+                        principalTable: "RecurringTransactions",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Link_Tag_RecurringTransactions",
+                columns: table => new
+                {
+                    TagId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    RecurringTransactionId = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Link_Tag_RecurringTransactions", x => new { x.RecurringTransactionId, x.TagId });
+                    table.ForeignKey(
+                        name: "FK_Link_Tag_RecurringTransactions_Categories_TagId",
+                        column: x => x.TagId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Link_Tag_RecurringTransactions_RecurringTransactions_RecurringTransactionId",
+                        column: x => x.RecurringTransactionId,
+                        principalTable: "RecurringTransactions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Files",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 255, nullable: false),
+                    Data = table.Column<byte[]>(type: "BLOB", nullable: false),
+                    AccountTransactionId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Filename = table.Column<string>(type: "TEXT", nullable: false),
+                    ContentType = table.Column<string>(type: "TEXT", nullable: false),
+                    Notes = table.Column<string>(type: "TEXT", nullable: false),
+                    CreatedOnUTC = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Files", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Files_AccountTransactions_AccountTransactionId",
+                        column: x => x.AccountTransactionId,
+                        principalTable: "AccountTransactions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Link_Categories_Transactions",
+                columns: table => new
+                {
+                    TagId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    AccountTransactionId = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Link_Categories_Transactions", x => new { x.AccountTransactionId, x.TagId });
+                    table.ForeignKey(
+                        name: "FK_Link_Categories_Transactions_AccountTransactions_AccountTransactionId",
+                        column: x => x.AccountTransactionId,
+                        principalTable: "AccountTransactions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Link_Categories_Transactions_Categories_TagId",
+                        column: x => x.TagId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AccountTransactions_AccountId",
+                table: "AccountTransactions",
+                column: "AccountId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AccountTransactions_RecurringTransactionId",
+                table: "AccountTransactions",
+                column: "RecurringTransactionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Files_AccountTransactionId",
+                table: "Files",
+                column: "AccountTransactionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Files_Name",
+                table: "Files",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Link_Categories_Transactions_TagId",
+                table: "Link_Categories_Transactions",
+                column: "TagId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Link_Tag_RecurringTransactions_TagId",
+                table: "Link_Tag_RecurringTransactions",
+                column: "TagId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RecurringTransactionGroups_Name",
+                table: "RecurringTransactionGroups",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RecurringTransactions_RecurringTransactionGroupId",
+                table: "RecurringTransactions",
+                column: "RecurringTransactionGroupId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Files");
+
+            migrationBuilder.DropTable(
+                name: "Link_Categories_Transactions");
+
+            migrationBuilder.DropTable(
+                name: "Link_Tag_RecurringTransactions");
+
+            migrationBuilder.DropTable(
+                name: "AccountTransactions");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Accounts");
+
+            migrationBuilder.DropTable(
+                name: "RecurringTransactions");
+
+            migrationBuilder.DropTable(
+                name: "RecurringTransactionGroups");
+        }
+    }
+}
