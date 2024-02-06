daa413c142ec4603bf89b21daa643f0eab7b9664
Kennith Mann <kmann@etherpunk.com>
2024-02-05 23:04:47 -0600

    Getting settings file handled

--- v9/MannsMoneyRegister/MainWindow.xaml.cs
+++ v9/MannsMoneyRegister/MainWindow.xaml.cs
@@ -1,15 +1,7 @@
-﻿using MannsMoneyRegister.Data;
-using System.Diagnostics;
-using System.Text;
+﻿using System.Configuration;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MannsMoneyRegister;
 
@@ -20,7 +12,7 @@ public partial class MainWindow : Window
 {
     public MainWindow()
     {
-        this.InitializeComponent();
+        InitializeComponent();
     }
 
     private void ribbonComboBox_Dashboard_AccountSelection_SelectionChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
--- v9/MannsMoneyRegister/MainWindowViewModel.cs
+++ v9/MannsMoneyRegister/MainWindowViewModel.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +12,80 @@ namespace MannsMoneyRegister
 {
     public class MainWindowViewModel
     {
+        private void SaveConfigValue(string key, string value)
+        {
+            try
+            {
+                var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                var settings = configFile.AppSettings.Settings;
+                if (settings[key] == null)
+                {
+                    settings.Add(key, value);
+                }
+                else
+                {
+                    settings[key].Value = value;
+                }
+                configFile.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+            }
+            catch (ConfigurationErrorsException ex)
+            {
+                Trace.WriteLine($"Error reading app.config: {ex}");
+            }
+        }
+
+        public string DatabaseLocation
+        {
+            get => ConfigurationManager.AppSettings["DatabaseLocation"] ?? "MMR.sqlite3";
+            set => SaveConfigValue("DatabaseLocation", value);
+        }
+
+        public Guid DefaultAccountId
+        {
+            get
+            {
+                if (Guid.TryParse(ConfigurationManager.AppSettings["DefaultAccountId"], out Guid id) == false)
+                {
+                    return Guid.Empty;
+                }
+                return id;
+            }
+            set => SaveConfigValue("DefaultAccountId", value.ToString());
+        }
+
+        public string DefaultDayCount
+        {
+            get => ConfigurationManager.AppSettings["DefaultDayCount"] ?? "45 days";
+            set => SaveConfigValue("DefaultDayCount", value.ToString());
+        }
+
+        public DateTime DefaultSearchDayCustomStart
+        {
+            get
+            {
+                if (DateTime.TryParse(ConfigurationManager.AppSettings["DefaultSearchDayCustomStart"], out DateTime start) == false)
+                {
+                    return DateTime.UtcNow.AddMonths(-1);
+                }
+                return start;
+            }
+            set => SaveConfigValue("DefaultSearchDayCustomStart", value.ToString());
+        }
+
+        public DateTime DefaultSearchDayCustomEnd
+        {
+            get
+            {
+                if (DateTime.TryParse(ConfigurationManager.AppSettings["DefaultSearchDayCustomEnd"], out DateTime start) == false)
+                {
+                    return DateTime.UtcNow;
+                }
+                return start;
+            }
+            set => SaveConfigValue("DefaultSearchDayCustomEnd", value.ToString());
+        }
+
 
     }
 }
