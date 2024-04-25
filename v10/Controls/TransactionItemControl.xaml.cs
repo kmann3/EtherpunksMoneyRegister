342c9ced664dbd815731f20395de657de58cdd85
Kennith Mann <kmann@etherpunk.com>
2024-04-25 00:31:00 -0500

    Work on the Transaction Control

--- v10/Controls/TransactionItemControl.xaml.cs
+++ v10/Controls/TransactionItemControl.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -21,14 +22,21 @@ namespace MannsMoneyRegister.Controls;
 /// </summary>
 public partial class TransactionItemControl : UserControl
 {
+    private static Regex RegEx_Numeric = new Regex(@"[0-9.$,-]");
+
     public class TransactionItem
     {
         public Guid Id { get; set; } = Guid.NewGuid();
         public string Name { get; set; } = string.Empty;
         public decimal Amount { get; set; } = 0m;
-        public string AmountString { get { return $"{Amount.ToString("C")}"; } }
         public decimal Balance { get; set; } = 0m;
-        public string BalanceString{ get { return $"{Balance.ToString("C")}"; } }
+        public bool IsPending
+        {
+            get
+            {
+                return PendingLocalTime == null && ClearedLocalTime != null;
+            }
+        }
         public DateTime? PendingLocalTime { get; set; } = null;
         public DateTime? ClearedLocalTime { get; set; } = null;
         private string _notes = string.Empty;
@@ -61,19 +69,88 @@ public partial class TransactionItemControl : UserControl
     }
     private bool _isAlternateRow = false;
 
-    private Brush _backgroundColor = Brushes.AliceBlue;
+    private Brush _backgroundCurrentColor = Brushes.AliceBlue;
+
+    /// <summary>
+    /// The primary background of the transaction.ds
+    /// </summary>
+    private Brush _backgroundPrimaryColor = Brushes.AliceBlue;
+
+    /// <summary>
+    /// The alternate background of the transaction.
+    /// </summary>
+    private Brush _backgroundAlternateColor = Brushes.Beige;
+
+    /// <summary>
+    /// This is used for transactions that are pending but not yet cleared.
+    /// </summary>
+    private Brush _background_pending = Brushes.DarkKhaki;
+
+    /// <summary>
+    ///  This is used if there is no pending and no cleared date. The transaction is reserved. This is usually used for things like bills or expected recurring transactions.
+    /// </summary>
+    private Brush _background_reserved = Brushes.Salmon;
 
     private void SetBackground()
     {
-        if (_isAlternateRow && Item.ClearedLocalTime == null && Item.PendingLocalTime == null) _backgroundColor = Brushes.Red;
-        else if (_isAlternateRow && Item.ClearedLocalTime == null && Item.PendingLocalTime.HasValue) _backgroundColor = Brushes.Peru;
-        else if (_isAlternateRow && Item.ClearedLocalTime.HasValue && Item.PendingLocalTime.HasValue) _backgroundColor = Brushes.Beige;
-        else if (!_isAlternateRow && Item.ClearedLocalTime == null && Item.PendingLocalTime == null) _backgroundColor = Brushes.Salmon;
-        else if (!_isAlternateRow && Item.ClearedLocalTime == null && Item.PendingLocalTime.HasValue) _backgroundColor = Brushes.Yellow;
+        if (Item.ClearedLocalTime == null && Item.PendingLocalTime.HasValue) _backgroundCurrentColor = _background_pending;
+        else if (Item.ClearedLocalTime == null && Item.PendingLocalTime == null) _backgroundCurrentColor = _background_reserved;
+        else if (_isAlternateRow) _backgroundCurrentColor = _backgroundAlternateColor;
+        else _backgroundCurrentColor = _backgroundPrimaryColor;
 
-        this.BackgroundBorder.Background = _backgroundColor;
-        this.TextBoxName.Background = _backgroundColor;
-        this.TextBoxAmount.Background = _backgroundColor;
+        this.BackgroundBorder.Background = _backgroundCurrentColor;
+        this.TextBoxName.Background = _backgroundCurrentColor;
+        this.TextBoxAmount.Background = _backgroundCurrentColor;
+        this.TextBoxNotes.Background = _backgroundCurrentColor;
+        this.TextBoxConfirmationNumber.Background = _backgroundCurrentColor;
+        this.TextBlockAttachments.Background = _backgroundCurrentColor;
+        //this.PendingDatePicker.Background = _backgroundCurrentColor;
+
+        if (Item.PendingLocalTime == null && Item.ClearedLocalTime == null)
+        {
+            // Reserved
+
+            // Pending
+            ButtonPendingTransaction.Visibility = Visibility.Visible;
+            TextBlockPendingLocalTime.Visibility = Visibility.Hidden;
+
+            // Cleared
+            ButtonClearedTransaction.Visibility = Visibility.Visible;
+            TextBlockClearedLocalTime.Visibility = Visibility.Hidden;
+        } else if(Item.PendingLocalTime != null && Item.ClearedLocalTime == null)
+        {
+            // Pending
+            // Pending
+            ButtonPendingTransaction.Visibility = Visibility.Hidden;
+            TextBlockPendingLocalTime.Visibility = Visibility.Visible;
+
+            // Cleared
+            ButtonClearedTransaction.Visibility = Visibility.Visible;
+            TextBlockClearedLocalTime.Visibility = Visibility.Hidden;
+
+        }
+        else
+        {
+            // Cleared
+
+            // Pending
+            ButtonPendingTransaction.Visibility = Visibility.Hidden;
+            TextBlockPendingLocalTime.Visibility = Visibility.Visible;
+
+            // Cleared
+            ButtonClearedTransaction.Visibility = Visibility.Hidden;
+            TextBlockClearedLocalTime.Visibility = Visibility.Visible;
+
+        }
+
+        if(Item.HasAttachments)
+        {
+            TextBlockAttachments.Visibility = Visibility.Visible;
+            TextBlockAttachments.ToolTip = "There are 12 attachments";
+        } else
+        {
+            TextBlockAttachments.Visibility = Visibility.Hidden;
+        }
     }
 
 
@@ -83,49 +160,60 @@ public partial class TransactionItemControl : UserControl
         this.DataContext = this;
     }
 
-    private void datagridButtonPendingTransaction_Click(object sender, RoutedEventArgs e)
+    private void ButtonPendingTransaction_Click(object sender, RoutedEventArgs e)
     {
-        Trace.WriteLine($"Pending Transaction Button Clicked: {Item.Name}");
+        this.Item.PendingLocalTime = DateTime.Now;
+        ButtonPendingTransaction.Visibility = Visibility.Hidden;
+        TextBlockPendingLocalTime.Visibility = Visibility.Visible;
+        TextBlockPendingLocalTime.Text = this.Item.PendingLocalTime.ToString();
+        SetBackground();
     }
 
-    private void datagridButtonClearTransaction_Click(object sender, RoutedEventArgs e)
+    private void ButtonClearTransaction_Click(object sender, RoutedEventArgs e)
     {
-        Trace.WriteLine($"Clear Transaction Button Clicked: {Item.Name}");
+        this.Item.ClearedLocalTime = DateTime.Now;
+        ButtonClearedTransaction.Visibility = Visibility.Hidden;
+        ButtonPendingTransaction.Visibility = Visibility.Hidden;
+        TextBlockClearedLocalTime.Visibility = Visibility.Visible;
+        TextBlockClearedLocalTime.Text = this.Item.ClearedLocalTime.ToString();
+        SetBackground();
     }
 
-    private void GridControl_MouseWheel(object sender, MouseWheelEventArgs e)
+
+    private void Object_GotFocus(object sender, RoutedEventArgs e)
     {
-        Trace.WriteLine($"Mouse Wheel: {e.Delta}");
+        if(sender is TextBox)
+            ((TextBox)(e.Source)).Background = Brushes.White;
     }
 
-    private void GridControl_MouseDown(object sender, MouseButtonEventArgs e)
+    private void Object_LostFocus(object sender, RoutedEventArgs e)
     {
-        Trace.WriteLine($"Mouse down {e.ClickCount}");
+        if (sender is TextBox)
+            ((TextBox)(e.Source)).Background = _backgroundCurrentColor;
     }
 
-    private void TextBoxName_GotFocus(object sender, RoutedEventArgs e)
+    private void TextBoxAmount_PreviewTextInput(object sender, TextCompositionEventArgs e)
     {
-        ((TextBox)(e.Source)).Background = Brushes.White;
+        e.Handled = !RegEx_Numeric.IsMatch(e.Text);
     }
 
-    private void TextBoxName_LostFocus(object sender, RoutedEventArgs e)
+    private void PendingTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
     {
-        ((TextBox)(e.Source)).Background = _backgroundColor;
+        //PendingCalendar.IsOpen = true;
     }
 
-    private void TextBoxAmount_GotFocus(object sender, RoutedEventArgs e)
+    private void OnMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
     {
-        ((TextBox)(e.Source)).Background = Brushes.White;
+
     }
 
-    private void TextBoxAmount_LostFocus(object sender, RoutedEventArgs e)
+    private void PendingDatePicker_GotFocus(object sender, RoutedEventArgs e)
     {
-        ((TextBox)(e.Source)).Background = _backgroundColor;
+
     }
 
-    private void TextBoxName_MouseWheel(object sender, MouseWheelEventArgs e)
+    private void PendingDatePicker_LostFocus(object sender, RoutedEventArgs e)
     {
-        // Fire the "user is scrolling" event
-        Trace.WriteLine($"{e.Delta}");
+
     }
 }
