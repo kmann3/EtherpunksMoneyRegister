425b153d7948ce2ceee5fce40d03d619b68d4001
Kenny Mann <kmann@etherpunk.com>
2025-01-04 10:55:45 -0600

    notes in dhasboar

--- v14/EtherpunksMoneyRegister.v14/EtherpunksMoneyRegister.v14/Views/DashboardView.swift
+++ v14/EtherpunksMoneyRegister.v14/EtherpunksMoneyRegister.v14/Views/DashboardView.swift
@@ -9,10 +9,20 @@ import SwiftUI
 
 struct DashboardView: View {
     var body: some View {
-        Text("Dashboard View")
+        VStack {
+            Text("Dashboard View")
+            Text("Current Available Spending")
+            Text("Reserved Transactions:")
+            Text("Pending Transactions:")
+        }
     }
 }
 
 #Preview {
     DashboardView()
+        .modelContainer(Previewer().container)
+        .environment(PathStore())
+#if os(macOS)
+        .frame(width: 900, height: 500)
+#endif
 }
