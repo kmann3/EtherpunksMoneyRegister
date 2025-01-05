7130479c3b43309750d8595bb57786090b1a7923
Kenny Mann <kmann@etherpunk.com>
2025-01-05 03:00:02 -0600

    Update EtherpunksMoneyRegister_v14App.swift
    run swift-format

--- v14/EtherpunksMoneyRegister.v14/EtherpunksMoneyRegister.v14/EtherpunksMoneyRegister_v14App.swift
+++ v14/EtherpunksMoneyRegister.v14/EtherpunksMoneyRegister.v14/EtherpunksMoneyRegister_v14App.swift
@@ -5,40 +5,40 @@
 //  Created by Kennith Mann on 11/15/24.
 //
 
-import SwiftUI
 import SwiftData
+import SwiftUI
 
 @main
 struct EtherpunksMoneyRegister_v14App: App {
-#if !DEBUG
-    var container: ModelContainer = {
-        let schema = Schema([
-            Account.self,
-            AccountTransaction.self,
-            RecurringGroup.self,
-            RecurringTransaction.self,
-            TransactionFile.self,
-            TransactionTag.self,
-        ])
-        // There is a problem with TransactionFile.Data and NOT being stored in memory.
-        let config = ModelConfiguration(isStoredInMemoryOnly: false)
+    #if !DEBUG
+        var container: ModelContainer = {
+            let schema = Schema([
+                Account.self,
+                AccountTransaction.self,
+                RecurringGroup.self,
+                RecurringTransaction.self,
+                TransactionFile.self,
+                TransactionTag.self,
+            ])
+            // There is a problem with TransactionFile.Data and NOT being stored in memory.
+            let config = ModelConfiguration(isStoredInMemoryOnly: false)
 
-        do {
-            return try ModelContainer(for: schema, configurations: [config])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
-#endif
+            do {
+                return try ModelContainer(for: schema, configurations: [config])
+            } catch {
+                fatalError("Could not create ModelContainer: \(error)")
+            }
+        }()
+    #endif
 
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
         #if DEBUG
-        .modelContainer(Previewer().container)
+            .modelContainer(Previewer().container)
         #else
-        .modelContainer(container)
+            .modelContainer(container)
         #endif
     }
 }
