7ad4fb099ca363f9f365f782127940993586f9ff
Kenny Mann <kmann@etherpunk.com>
2025-01-08 05:12:34 -0600

    Commenting out link tables

--- v14/EtherpunksMoneyRegister.v14/EtherpunksMoneyRegister.v14/Data/MoneyData/Tables/Link_RecurringTransaction_RecurringTransactionTag.swift
+++ v14/EtherpunksMoneyRegister.v14/EtherpunksMoneyRegister.v14/Data/MoneyData/Tables/Link_RecurringTransaction_RecurringTransactionTag.swift
@@ -5,27 +5,27 @@
 //  Created by Kennith Mann on 8/19/24.
 //
 
-import Foundation
-import SwiftData
-
-@Model
-final class Link_RecurringTransaction_RecurringTransactionTag: ObservableObject, CustomDebugStringConvertible, Identifiable, Hashable
-{
-    public var recurringTransactionId: UUID? = nil
-    public var recurringTransactionTagId: UUID? = nil
-    public var createdOnUTC: Date = Date()
-
-    public var debugDescription: String {
-        return """
-            RecurringTransactionTag:
-            - recurringTransactionId: \(String(describing: recurringTransactionId))
-            - recurringTransactionTagId: \(String(describing: recurringTransactionTagId))
-            - createdOnUTC: \(createdOnUTC)
-            """
-    }
-
-    init(recurringTransactionId: UUID, recurringTransactionTagId: UUID) {
-        self.recurringTransactionId = recurringTransactionId
-        self.recurringTransactionTagId = recurringTransactionTagId
-    }
-}
+//import Foundation
+//import SwiftData
+//
+//@Model
+//final class Link_RecurringTransaction_RecurringTransactionTag: ObservableObject, CustomDebugStringConvertible, Identifiable, Hashable
+//{
+//    public var recurringTransactionId: UUID? = nil
+//    public var recurringTransactionTagId: UUID? = nil
+//    public var createdOnUTC: Date = Date()
+//
+//    public var debugDescription: String {
+//        return """
+//            RecurringTransactionTag:
+//            - recurringTransactionId: \(String(describing: recurringTransactionId))
+//            - recurringTransactionTagId: \(String(describing: recurringTransactionTagId))
+//            - createdOnUTC: \(createdOnUTC)
+//            """
+//    }
+//
+//    init(recurringTransactionId: UUID, recurringTransactionTagId: UUID) {
+//        self.recurringTransactionId = recurringTransactionId
+//        self.recurringTransactionTagId = recurringTransactionTagId
+//    }
+//}
--- v14/EtherpunksMoneyRegister.v14/EtherpunksMoneyRegister.v14/Data/MoneyData/Tables/Link_Transaction_TransactionTag.swift
+++ v14/EtherpunksMoneyRegister.v14/EtherpunksMoneyRegister.v14/Data/MoneyData/Tables/Link_Transaction_TransactionTag.swift
@@ -5,26 +5,26 @@
 //  Created by Kennith Mann on 8/19/24.
 //
 
-import Foundation
-import SwiftData
-
-@Model
-final class Link_Transaction_TransactionTag: ObservableObject, CustomDebugStringConvertible, Identifiable, Hashable {
-    public var transactionId: UUID? = nil
-    public var transactionTagId: UUID? = nil
-    public var createdOnUTC: Date = Date()
-
-    public var debugDescription: String {
-        return """
-            RecurringTransactionTag:
-            - transactionId: \(String(describing: transactionId))
-            - transactionTagId: \(String(describing: transactionTagId))
-            - createdOnUTC: \(createdOnUTC)
-            """
-    }
-
-    init(transactionId: UUID, transactionTagId: UUID) {
-        self.transactionId = transactionId
-        self.transactionTagId = transactionTagId
-    }
-}
+//import Foundation
+//import SwiftData
+//
+//@Model
+//final class Link_Transaction_TransactionTag: ObservableObject, CustomDebugStringConvertible, Identifiable, Hashable {
+//    public var transactionId: UUID? = nil
+//    public var transactionTagId: UUID? = nil
+//    public var createdOnUTC: Date = Date()
+//
+//    public var debugDescription: String {
+//        return """
+//            RecurringTransactionTag:
+//            - transactionId: \(String(describing: transactionId))
+//            - transactionTagId: \(String(describing: transactionTagId))
+//            - createdOnUTC: \(createdOnUTC)
+//            """
+//    }
+//
+//    init(transactionId: UUID, transactionTagId: UUID) {
+//        self.transactionId = transactionId
+//        self.transactionTagId = transactionTagId
+//    }
+//}
