e1093697fd01eeefe7a9128cc20ac6a99c25cdd1
Kenny Mann <kmann@etherpunk.com>
2026-02-12 17:17:41 -0600

    Add ignore for xcuserstate and work on detailViewBuilder

--- v16/EtherpunksMoneyRegister.v16/EtherpunksMoneyRegister.v16/Views/ContentView/Detail_ViewBuilder.swift
+++ v16/EtherpunksMoneyRegister.v16/EtherpunksMoneyRegister.v16/Views/ContentView/Detail_ViewBuilder.swift
@@ -10,5 +10,48 @@ import SwiftUI
 extension ContentView {
     @ViewBuilder
     internal func detailViewBuilder(for route: PathStore.Route) -> some View {
+        switch route {
+        case .account_Create:
+            Text("TBI - Account Create")
+        case .account_List:
+            Text("Account List")
+        case .account_Edit(let account): Text("Account Edit \(account.name)")
+            //AccountEditView(account) { action in self.changeRoute(action) }
+        case .account_Details(let account): Text("Account Details: \(account.name)")
+            //AccountDetailView(account) { action in self.changeRoute(action) }
+        case .dashboard: Text("TBI - Dashboard")
+        case .recurringGroup_Create:
+            Text("TBI - Recurring Group Create")
+        case .recurringGroup_Details(let recGroup): Text("TBI - Recurring Group Details: \(recGroup.name)")
+            //ReserveGroupView(recGroup) { action in self.changeRoute(action) }
+        case .recurringGroup_Edit(let recGroup):
+            Text("TBI - Recurring Group Edit: \(recGroup.name)")
+        case .recurringGroup_List(let recGroup): Text("TBI - Recurring Group List")
+            //RecurringGroupListView(recGroup) { action in self.changeRoute(action) }
+        case .recurringGroup_Reserve(let recGroup): Text("TBI - Recurring Group Reserve: \(recGroup.name)")
+            //ReserveGroupView(recGroup) { action in self.changeRoute(action) }
+        case .recurringTransaction_Create: Text("TBI - Recurring Transaction Create")
+        case .recurringTransaction_Create_FromTran(let tranID): Text("TBI - Recurring Transaction Create From Transaction: \(tranID.name)")
+        case .recurringTransaction_Details(let recTran): Text("Recurring Transaction Details \(recTran.name)")
+        case .recurringTransaction_Edit(let recTran):
+            Text("TBI - Recurring Transaction Edit: \(recTran.name)")
+        case .recurringTransaction_List(let recTran): Text("TBI - Recurring Transaction List")
+            //RecurringTransactionListView(recTran) { action in self.changeRoute(action) }
+        case .recurringTransaction_Reserve(let recTran): Text("TBI - Recurring Transaction Reserve: \(recTran.name)")
+        case .report_Tax: Text("TBI - Report Tax")
+        case .search: Text("TBI - Search")
+        case .settings: Text("TBI - Settings")
+        case .tag_Create: Text("TBI - Tag Create")
+        case .tag_Details(let tag): Text("TBI - Tag Details: \(tag.name)")
+            //TagDetailView(tag: tag) { action in self.changeRoute(action) }
+        case .tag_Edit(let tag): Text("TBI - Tag Edit: \(tag.name)")
+        case .tag_List: Text("TBI - Tag List")
+        case .transaction_Create: Text("TBI - Transaction Create")
+        case .transaction_Detail(let transaction): Text("Transaction Detail \(transaction.name)")
+            //AccountTransactionDetailsView(transaction) { action in self.changeRoute(action) }
+        case .transaction_Edit(let transaction): Text("Transaction Edit \(transaction.name)")
+        case .transaction_List(let account): Text("Transaction List")
+            //AccountTransactionListView(account) { action in self.changeRoute(action) }
+        }
     }
 }
